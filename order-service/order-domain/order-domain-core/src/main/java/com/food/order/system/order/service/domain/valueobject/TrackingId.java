fe21f2793a4c51433b9b163dade7530c1f28912f
mselvi <mselvi@iski.gov.tr>
2023-12-09 17:34:00 +0300

    added street adress and trackingid value objects to order domain core

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/order/system/order/service/domain/valueobject/TrackingId.java
@@ -0,0 +1,16 @@
+package com.food.order.system.order.service.domain.valueobject;
+
+import com.food.order.system.domain.valueobject.BaseId;
+
+import java.util.UUID;
+
+/**
+ * @Author mselvi
+ * @Created 09.12.2023
+ */
+
+public class TrackingId extends BaseId<UUID> {
+    protected TrackingId(UUID value) {
+        super(value);
+    }
+}
