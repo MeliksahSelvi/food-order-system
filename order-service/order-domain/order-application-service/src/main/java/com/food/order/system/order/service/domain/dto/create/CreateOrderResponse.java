9b7d871be339a0a4e5675fbd946b031185be8792
mselvi <mselvi@iski.gov.tr>
2023-12-16 15:57:25 +0300

    added order order command and response dto classes to application-service

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/order/system/order/service/domain/dto/create/CreateOrderResponse.java
@@ -0,0 +1,26 @@
+package com.food.order.system.order.service.domain.dto.create;
+
+import com.food.order.system.domain.valueobject.OrderStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+/**
+ * @Author mselvi
+ * @Created 16.12.2023
+ */
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CreateOrderResponse {
+    @NotNull
+    private final UUID orderTrackingId;
+    @NotNull
+    private final OrderStatus orderStatus;
+    @NotNull
+    private final String message;
+}
