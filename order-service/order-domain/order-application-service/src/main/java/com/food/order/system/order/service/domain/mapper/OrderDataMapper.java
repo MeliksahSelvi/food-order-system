63ce2a87a8c743c6c3e231339e2c85f5a1f10a18
mselvi <mselvi@iski.gov.tr>
2023-12-16 16:47:10 +0300

    added data mapper for order aggregate root

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/order/system/order/service/domain/mapper/OrderDataMapper.java
@@ -0,0 +1,12 @@
+package com.food.order.system.order.service.domain.mapper;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author mselvi
+ * @Created 16.12.2023
+ */
+
+@Component
+public class OrderDataMapper {
+}
