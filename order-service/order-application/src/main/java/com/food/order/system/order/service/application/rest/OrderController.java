7b2d8879b6cd890fd206335b41d0b277e9c5b16d
mselvi <mselvi@iski.gov.tr>
2023-12-18 17:07:48 +0300

    added order domain layer exception handler and order endpoint apis to be used client

---
+++ order-service/order-application/src/main/java/com/food/order/system/order/service/application/rest/OrderController.java
@@ -0,0 +1,42 @@
+package com.food.order.system.order.service.application.rest;
+
+import com.food.order.system.order.service.domain.dto.create.CreateOrderCommand;
+import com.food.order.system.order.service.domain.dto.create.CreateOrderResponse;
+import com.food.order.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.order.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.order.system.order.service.domain.ports.input.service.OrderApplicationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+/**
+ * @Author mselvi
+ * @Created 18.12.2023
+ */
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/orders", produces = "application/vnd.api.v1+json")
+public class OrderController {
+
+    private final OrderApplicationService orderApplicationService;
+
+    @PostMapping
+    public ResponseEntity<CreateOrderResponse> createOrder(@RequestBody CreateOrderCommand createOrderCommand) {
+        log.info("Creating order for customer: {} at restaurant: {}", createOrderCommand.getCustomerId(), createOrderCommand.getRestaurantId());
+        CreateOrderResponse createOrderResponse = orderApplicationService.createOrder(createOrderCommand);
+        log.info("Order created with tracking id: {}", createOrderResponse.getOrderTrackingId());
+        return ResponseEntity.ok(createOrderResponse);//todo neden 201 dönmedik?
+    }
+
+    @GetMapping("/{trackingId}")
+    public ResponseEntity<TrackOrderResponse> getOrderByTrackingId(@PathVariable UUID trackingId) {
+        TrackOrderResponse trackOrderResponse = orderApplicationService.trackOrder(TrackOrderQuery.builder().orderTrackingId(trackingId).build());
+        log.info("Returnin order status with tracking id: {}", trackOrderResponse.getOrderTrackingId());
+        return ResponseEntity.ok(trackOrderResponse);
+    }
+}
