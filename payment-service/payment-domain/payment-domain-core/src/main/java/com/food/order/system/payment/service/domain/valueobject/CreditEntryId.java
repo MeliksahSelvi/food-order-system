c106d6030eb983ddc8dd1ae6685302b1b06c6999
mselvi <mselvi@iski.gov.tr>
2023-12-19 23:33:33 +0300

    added payment aggregate roots value objects

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/food/order/system/payment/service/domain/valueobject/CreditEntryId.java
@@ -0,0 +1,16 @@
+package com.food.order.system.payment.service.domain.valueobject;
+
+import com.food.order.system.domain.valueobject.BaseId;
+
+import java.util.UUID;
+
+/**
+ * @Author mselvi
+ * @Created 19.12.2023
+ */
+
+public class CreditEntryId extends BaseId<UUID> {
+    public CreditEntryId(UUID value) {
+        super(value);
+    }
+}
