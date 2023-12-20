1e3d9e192fe9402120a647715fe49c2f69a364e1
mselvi <mselvi@iski.gov.tr>
2023-12-20 10:24:07 +0300

    added primary adapter payment application service

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/order/system/payment/service/domain/PaymentRequestMessageListenerImpl.java
@@ -0,0 +1,45 @@
+package com.food.order.system.payment.service.domain;
+
+import com.food.order.system.payment.service.domain.dto.PaymentRequest;
+import com.food.order.system.payment.service.domain.event.PaymentEvent;
+import com.food.order.system.payment.service.domain.ports.input.message.listener.PaymentRequestMessageListener;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author mselvi
+ * @Created 20.12.2023
+ */
+
+/*
+ * Bu listener (primary adapter) order bounded context'i tarafından gönderilen eventi takip eden messaging yapısının
+ * çağıracağı input portunun implementation'u
+ * */
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PaymentRequestMessageListenerImpl implements PaymentRequestMessageListener {
+
+    private final PaymentRequestHelper paymentRequestHelper;
+
+    @Override
+    public void completePayment(PaymentRequest paymentRequest) {
+        PaymentEvent paymentEvent = paymentRequestHelper.persistPayment(paymentRequest);
+        fireEvent(paymentEvent);
+    }
+
+    @Override
+    public void cancelPayment(PaymentRequest paymentRequest) {
+        PaymentEvent paymentEvent = paymentRequestHelper.persistCancelPayment(paymentRequest);
+        fireEvent(paymentEvent);
+    }
+
+    private void fireEvent(PaymentEvent paymentEvent) {
+        log.info("Publishing payment event with payment id: {} and order id: {}",
+                paymentEvent.getPayment().getId().getValue(),
+                paymentEvent.getPayment().getOrderId().getValue());
+
+        paymentEvent.fire();
+    }
+}
