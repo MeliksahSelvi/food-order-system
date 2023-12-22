e6d542b174579c917e00d5d33e90ef016b97c84f
mselvi <mselvi@iski.gov.tr>
2023-12-22 17:48:19 +0300

    added order approval outbox models

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/order/system/order/service/domain/outbox/model/approval/OrderApprovalOutboxMessage.java
@@ -0,0 +1,51 @@
+package com.food.order.system.order.service.domain.outbox.model.approval;
+
+import com.food.order.system.domain.valueobject.OrderStatus;
+import com.food.order.system.outbox.OutboxStatus;
+import com.food.order.system.saga.SagaStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+/**
+ * @Author mselvi
+ * @Created 22.12.2023
+ */
+
+/*
+ * restauran_approval_outbox tablosunun verileri
+ * */
+@Getter
+@Builder
+@AllArgsConstructor
+public class OrderApprovalOutboxMessage {
+    private UUID id;
+    private UUID sagaId;
+    private ZonedDateTime createdAt;
+    private ZonedDateTime processedAt;
+    private String type;
+    private String payload;
+    private SagaStatus sagaStatus;
+    private OutboxStatus outboxStatus;
+    private OrderStatus orderStatus;
+    private int version;
+
+    public void setProcessedAt(ZonedDateTime processedAt) {
+        this.processedAt = processedAt;
+    }
+
+    public void setSagaStatus(SagaStatus sagaStatus) {
+        this.sagaStatus = sagaStatus;
+    }
+
+    public void setOutboxStatus(OutboxStatus outboxStatus) {
+        this.outboxStatus = outboxStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+}
