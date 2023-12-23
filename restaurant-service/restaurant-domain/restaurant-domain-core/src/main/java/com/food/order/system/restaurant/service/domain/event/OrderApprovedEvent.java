9c1d32a5af856962c649b4194878f15a51b6dcc3
mselvi <mselvi@iski.gov.tr>
2023-12-24 02:19:21 +0300

    changed restaurant domain modules events

--- restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/order/system/restaurant/service/domain/event/OrderApprovedEvent.java
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/order/system/restaurant/service/domain/event/OrderApprovedEvent.java
@@ -1,6 +1,5 @@
 package com.food.order.system.restaurant.service.domain.event;
 
-import com.food.order.system.domain.event.publisher.DomainEventPublisher;
 import com.food.order.system.domain.valueobject.RestaurantId;
 import com.food.order.system.restaurant.service.domain.entity.OrderApproval;
 
@@ -13,20 +12,10 @@ import java.util.List;
  */
 
 public class OrderApprovedEvent extends OrderApprovalEvent {
-
-    private final DomainEventPublisher<OrderApprovedEvent> orderApprovedEventDomainEventPublisher;
-
     public OrderApprovedEvent(OrderApproval orderApproval,
                               RestaurantId restaurantId,
                               List<String> failureMessages,
-                              ZonedDateTime createdAt,
-                              DomainEventPublisher<OrderApprovedEvent> orderApprovedEventDomainEventPublisher) {
+                              ZonedDateTime createdAt) {
         super(orderApproval, restaurantId, failureMessages, createdAt);
-        this.orderApprovedEventDomainEventPublisher = orderApprovedEventDomainEventPublisher;
-    }
-
-    @Override
-    public void fire() {
-        this.orderApprovedEventDomainEventPublisher.publish(this);
     }
 }
